6ddd85160038f9f257c94dd0d545bc1bad7b12b3
LZYBJB <1415790333@qq.com>
2025-10-14 21:28:04 +0800

    日常开发

--- Assets/Scripts/UI/UIItem_HaloPanel.cs
+++ Assets/Scripts/UI/UIItem_HaloPanel.cs
@@ -1,11 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Moyo.Unity;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UIItem_HaloPanel : MonoBehaviour
 {
-    public Toggle toggle;
+    [AutoBind] public Toggle toggle_环境;
+    [AutoBind] public Toggle toggle_治安;
+    [AutoBind] public Toggle toggle_医疗;
 
-    
+
+    private List<Func<Vector3Int, bool>> currentConditions = new List<Func<Vector3Int, bool>>(15);
+
+    private void Reset()
+    {
+        this.AutoBindFields();
+    }
+
+    private void Awake()
+    {
+
+
+        toggle_环境.onValueChanged.AddListener((b) =>
+        {
+
+        });
+    }
+
+
+    private void Refresh()
+    {
+
+    }
+
+
 }
